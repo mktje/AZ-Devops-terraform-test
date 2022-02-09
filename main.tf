8bf381ba2e58698eed8d3354548e539a511ffc7d
mktje <mail@martinkruijff.nl>
2022-02-09 11:27:11 +0100

    updated main.tf res group del

--- main.tf
+++ main.tf
@@ -1,49 +1,44 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "2.95.0"
     }
   }
   backend "azurerm" {
-    resource_group_name = "RG-terraform-test"
+    resource_group_name  = "RG-terraform-test"
     storage_account_name = "samkterraformtest"
-    container_name = "tfstate"
-    key = "terraform.tfstate"
-    
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+
   }
 }
 
 provider "azurerm" {
-    features {    
-    }
-}
-
-resource "azurerm_resource_group" "RG-terraform-test" {
-    name = "RG-terraform-test"
-    location = "westeurope"
+  features {
+  }
 }
 
 resource "azurerm_container_group" "CG-terraform-test" {
-    name = "CG-weather-api"
-    location = azurerm_resource_group.RG-terraform-test.location
-    resource_group_name = azurerm_resource_group.RG-terraform-test.name  
+  name                = "CG-weather-api"
+  location            = azurerm_resource_group.RG-terraform-test.location
+  resource_group_name = azurerm_resource_group.RG-terraform-test.name
 
-    ip_address_type     = "public"
-    dns_name_label      = "mktestweatherapi"
-    os_type             = "Linux"
+  ip_address_type = "public"
+  dns_name_label  = "mktestweatherapi"
+  os_type         = "Linux"
 
-    container {
-        name            = "weatherapi"
-        image           = "mktje/weatherapi"
-        cpu             = "1"
-        memory          = "1"
+  container {
+    name   = "weatherapi"
+    image  = "mktje/weatherapi"
+    cpu    = "1"
+    memory = "1"
 
-        ports {
-            port        = 80
-            protocol    = "TCP"
-        }
+    ports {
+      port     = 80
+      protocol = "TCP"
     }
+  }
 }
 
 
